0624900030ae54a3ad1be6f65237fbd0cfa37197
Lachie David <lachiepd@gmail.com>
2023-05-16 21:48:28 +1000

    changed to ES6 exports

--- src/api/api.js
+++ src/api/api.js
@@ -1,28 +1,20 @@
 const url = process.env.REACT_APP_SQUIGGLE_URL;
 const todaysDate = new Date();
-const teamLoader = async() =>{
+export const teamLoader = async() =>{
 	const response = await fetch(url + "/?q=teams");
 	const data = await response.json();
 	return data.teams;
 }
-
-const ladderLoader = async() => {
-
+export const ladderLoader = async() => {
 	const year = todaysDate.getFullYear();
 	const response = await fetch(url+"?q=standings");
 	const data = await response.json();
 	return data.standings;
-
 }
 
-const teamPageLoader = async(id) =>{
+export const teamPageLoader = async(id) =>{
 	const year = todaysDate.getFullYear();
 	const res = await fetch(url+"?q=games;"+"year="+year+";team="+id);
 	const data = await res.json();
 	return data;
-}
-module.exports={
-	teamLoader,
-	ladderLoader,
-	teamPageLoader,
 }
